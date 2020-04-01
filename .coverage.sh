488d3eaf1e44dd7dec6a256b9a7c4a590615006c
Survy Vaish <survy@cobaltrobotics.com>
2020-04-01 15:37:55 -0700

    wip

--- .coverage.sh
+++ .coverage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# Run coverage target and upload results
+
 source "${ICI_SRC_PATH}/workspace.sh"
 source "${ICI_SRC_PATH}/util.sh"
-
-# build coverage and upload
 
 for pkg in $COVERAGE_PKGS; do
 
@@ -11,12 +11,13 @@ for pkg in $COVERAGE_PKGS; do
 
     ws=~/target_ws
     extend="/opt/ros/$ROS_DISTRO"
-    ici_exec_in_workspace "$extend" "$ws" catkin build $pkg -v --no-deps --catkin-make-args ${pkg}_coverage_report  # trigger the coverage target
+    ici_exec_in_workspace "$extend" "$ws" catkin build $pkg -v --no-deps --catkin-make-args ${pkg}_coverage_report
 
     echo "Uploading coverage results to codecov.io"
 
     # Codecov will merge all these reports
     bash <(curl -s https://codecov.io/bash) -s /root/.ros
 
-    # Note: c++ output ends up in a different folder, see robot calibration repo
+    # TODO: Grab C++ coverage report as well.
+    #       See robot_calibration repo for example. The C++ coverage files are in a different location.
 done
