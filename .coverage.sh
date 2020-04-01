d4bbcdf4cd29efedc8047abdb9c535412aea4aef
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2020-04-01 15:52:40 -0700

    Documentation for how to run locally (#6)

--- .coverage.sh
+++ .coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Run coverage target and upload results
+
 source "${ICI_SRC_PATH}/workspace.sh"
 source "${ICI_SRC_PATH}/util.sh"
 
@@ -16,4 +18,6 @@ for pkg in $COVERAGE_PKGS; do
     # Codecov will merge all these reports
     bash <(curl -s https://codecov.io/bash) -s /root/.ros
 
+    # TODO: Grab C++ coverage report as well.
+    #       See robot_calibration repo for example. The C++ coverage files are in a different location.
 done
