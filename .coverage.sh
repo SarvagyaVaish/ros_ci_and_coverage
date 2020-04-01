71557f4112fddd6dad72d5d740d9a9cb481fd776
Survy Vaish <survy@cobaltrobotics.com>
2020-04-01 13:35:07 -0700

    no-op comments

--- .coverage.sh
+++ .coverage.sh
@@ -3,17 +3,20 @@
 source "${ICI_SRC_PATH}/workspace.sh"
 source "${ICI_SRC_PATH}/util.sh"
 
+# build coverage and upload
+
 for pkg in $COVERAGE_PKGS; do
 
     echo "Generating coverage for $pkg"
 
     ws=~/target_ws
     extend="/opt/ros/$ROS_DISTRO"
-    ici_exec_in_workspace "$extend" "$ws" catkin build $pkg -v --no-deps --catkin-make-args ${pkg}_coverage_report
+    ici_exec_in_workspace "$extend" "$ws" catkin build $pkg -v --no-deps --catkin-make-args ${pkg}_coverage_report  # trigger the coverage target
 
     echo "Uploading coverage results to codecov.io"
 
     # Codecov will merge all these reports
     bash <(curl -s https://codecov.io/bash) -s /root/.ros
 
+    # Note: c++ output ends up in a different folder, see robot calibration repo
 done
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 TravisCI: [![Build Status](https://travis-ci.org/SarvagyaVaish/ros_ci_and_coverage.svg?branch=master)](https://travis-ci.org/SarvagyaVaish/ros_ci_and_coverage)
 
 Codecov: [![codecov](https://codecov.io/gh/SarvagyaVaish/ros_ci_and_coverage/branch/master/graph/badge.svg)](https://codecov.io/gh/SarvagyaVaish/ros_ci_and_coverage)
+
+
+rosdep install --from-paths src --ignore-src -r -y
